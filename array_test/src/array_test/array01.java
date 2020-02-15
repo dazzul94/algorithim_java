e7841383ca06d60018c746d503c25d5729482608
dazzul94 <32053060+dazzul94@users.noreply.github.com>
2020-02-15 19:15:28 +0900

    create test codes

---
+++ array_test/src/array_test/array01.java
@@ -0,0 +1,9 @@
+package array_test;
+
+public class array01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!");
+	}
+}
---
+++ array_test2/src/array_test2/array02.java
@@ -0,0 +1,9 @@
+package array_test2;
+
+public class array02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, world!!@");
+	}
+}
